4323a07a4f22a80a25b8b5bab723a8f767f496fe
Praful <prafulrakhade02@gmail.com>
2022-04-29 12:30:41 +0530

    [praful] Added UC-1-Employee status program file

---
+++ EmpWageComputation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EmpWageComputation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------- WELCOME TO THE EMPLOYEE WAGE COMPUTATION ------------");
+            Console.WriteLine();
+            int isPresent = 1;
+            Random random = new Random();
+            int randomCheck = random.Next(2);
+            if(randomCheck==isPresent)
+            {
+                Console.WriteLine("The Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("The Employee is Absent");
+            }
+        }
+    }
+}
